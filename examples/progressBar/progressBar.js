519b07c7a2832f80d8929e6091bb3aaad096c088
TrickyPR <trickypr@users.noreply.github.com>
2020-06-14 18:39:37 +1000

    Add progress bar example

---
+++ examples/progressBar/progressBar.js
@@ -0,0 +1,31 @@
+import { MutationComponent, html } from 'https://unpkg.com/stera@1.0.0/dist/index.js'
+
+class ProgressBar extends MutationComponent {
+	render() {
+		const progressWidth = this.getAttribute('progress')
+
+		return html`
+			<div class="progress container">
+				<div class="progress bar" style="width: ${progressWidth}%"></div>
+
+				<style>
+					:root {
+						width: 100%;
+					}
+
+					.progress.container {
+						width: 100%;
+						background-color: #818AA350;
+					}
+
+					.progress.bar {
+						width: 1%;
+						height: 30px;
+						background: #19539F;
+					}
+				</style>
+			</div>
+		`
+	}
+}
+customElements.define('progress-bar', ProgressBar)
